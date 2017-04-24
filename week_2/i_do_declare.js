b4c7f124c1f2c6d71f084d3ce0a3d1275811fb0d
cmarr31 <chris@sitefuelusa.com>
2017-04-24 11:59:16 -0500

    Declare variables and print each.

---
+++ week_2/i_do_declare.js
@@ -0,0 +1,24 @@
+var num1 = 1;
+var num2 = 8;
+var num3 = -1;
+var num4 = 0.2;
+
+var str1 = "I";
+var str2 = "Do";
+var str3 = "Declare!";
+
+var x = true;
+var y = false;
+
+var u;
+
+console.log("The first number:", num1);
+console.log("The second number:" + num2);
+console.log("The third number:" + num3);
+console.log("The fourth number:" + num4);
+console.log("The first string:" + str1);
+console.log("The second string:" + str2);
+console.log("The third string:" + str3);
+console.log("Boolean 1:" + x);
+console.log("Boolean 2:" + y);
+console.log("The undefined:" + u);
